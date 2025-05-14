ba44f0ee0d072473554f0692a32677d4d2b34d3e
joaovpt <joaotassi18@gmail.com>
2025-05-14 12:36:49 -0300

    test: add import.sql and remove.sql scripts for test

---
+++ src/test/resources/import.sql
@@ -0,0 +1,11 @@
+DELETE FROM todos;
+
+INSERT INTO todos (id, nome, descricao, realizado, prioridade) VALUES (9995, 'Todo 1', 'Todo 1', false, 1);
+
+INSERT INTO todos (id, nome, descricao, realizado, prioridade) VALUES (9996, 'Todo 2', 'Todo 2', false, 1);
+
+INSERT INTO todos (id, nome, descricao, realizado, prioridade) VALUES (9997, 'Todo 3', 'Todo 3', false, 1);
+
+INSERT INTO todos (id, nome, descricao, realizado, prioridade) VALUES (9998, 'Todo 4', 'Todo 4', false, 1);
+
+INSERT INTO todos (id, nome, descricao, realizado, prioridade) VALUES (9999, 'Todo 5', 'Todo 5', false, 1);
